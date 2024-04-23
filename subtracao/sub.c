768160e296783240d81594e028153d4ba469bd9b
marivwz <mariana.fernandes42@etec.sp.gov.br>
2024-04-23 16:35:16 -0300

    alteração2

---
+++ subtracao/sub.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void main(){
+	int numero1;
+	int numero2;
+	int resultado;
+
+	printf("==Calculo de Subtração==");
+	puts("\nDigite um valor:");
+	scanf("%d",&numero1);
+	printf("\nDigite outro valor:");
+	scanf("%d",&numero2);
+	resultado = numero1-numero2;
+	printf("\n A subtração de %d e %d, resulta em: %d",numero1,numero2,resultado);
+
+
+
+
+
+
+
+
+}
